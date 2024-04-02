aaece1561648758692c64f894f6083e6ab239f64
Daniel Cardenas <danielcardeenas@gmail.com>
2024-04-01 17:18:49 -0700

    Init vite new page

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import "./app.css";
+
+export function App() {
+  return (
+    <>
+      <h1>Daniel Cardenas</h1>
+      <div class="card">
+        <p>
+          Edit <code>src/app.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
