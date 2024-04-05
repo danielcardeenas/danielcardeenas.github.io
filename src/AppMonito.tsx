1d9b74b8e5028399fa706812db4410ea53065647
Daniel Cardenas <danielcardeenas@gmail.com>
2024-04-05 15:48:31 -0700

    Use monito

--- src/AppMonito.tsx
+++ src/AppMonito.tsx
@@ -1,13 +1,25 @@
 import Spline from '@splinetool/react-spline';
 import './App.css';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
-    <>
-      <div className="display:flex h:100% w:100%">
-        <Spline scene="https://prod.spline.design/PpytwpWNYXgzJyiI/scene.splinecode" />
+    <div className="h:100%">
+      <div className="position:absolute w:100%">
+        <div className="display:flex jc:center ai:start">
+          <motion.h1
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: 'easeOut' }}
+            className="font-size:2.5rem mt:36px"
+          >
+            Daniel Cardenas
+          </motion.h1>
+        </div>
       </div>
-    </>
+      <div className="position:absolute w:100% bottom:0"></div>
+      <Spline scene="https://prod.spline.design/PpytwpWNYXgzJyiI/scene.splinecode" />
+    </div>
   );
 }
 
--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,41 @@
+import { motion } from 'framer-motion';
 import './App.css';
 import Spline from '@splinetool/react-spline';
+import { useState } from 'react';
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
   return (
-    <>
-      <div className="display:flex h:100% flex-direction:col flex-direction:row@sm w:100% min-h:500px">
-        <header className="gradient-bg px:14px w:70%@md w:100%@sm color:white">
-          <h1 className="font-size:1.5rem">Daniel Cardenas</h1>
-        </header>
-
-        {/* CPU 3D Model */}
-        <div className="py:30x px:8px w:30%@md w:100%@sm min-h:500px max-h:500px">
-          <Spline scene="https://prod.spline.design/lI9F1J1LQf34N-nN/scene.splinecode" />
+    <div className="h:100% m:24px br:24px">
+      {/* CPU 3D Model */}
+      <motion.div
+        style={{
+          visibility: loaded ? 'visible' : 'hidden',
+        }}
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: 'easeOut' }}
+        className="z:1 position:absolute bottom:2rem right:2rem w:20%@md w:100%@sm h:256px"
+      >
+        <Spline
+          onLoad={() => setLoaded(true)}
+          scene="https://prod.spline.design/lI9F1J1LQf34N-nN/scene.splinecode"
+        />
+      </motion.div>
+      <div className="display:flex gap:8px h:100% flex-direction:col flex-direction:row@sm w:100%">
+        <div className="gradient-bg h:100% px:14px w:100%@md w:100%@sm color:white">
+          <motion.h1
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: 'easeOut' }}
+            className="font-size:1.5rem"
+          >
+            Daniel Cardenas
+          </motion.h1>
         </div>
+        {/* <div className="grid py:30x px:8px w:30%@md w:100%@sm"></div> */}
       </div>
-    </>
+    </div>
   );
 }
 
