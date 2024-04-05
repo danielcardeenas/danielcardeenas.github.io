ede7c6bb8494630645d92bbc265d57f5894bf4a5
Daniel Cardenas <danielcardeenas@gmail.com>
2024-04-05 13:44:08 -0700

    Add home

---
+++ src/AppMonito.tsx
@@ -0,0 +1,14 @@
+import Spline from '@splinetool/react-spline';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="display:flex h:100% w:100%">
+        <Spline scene="https://prod.spline.design/PpytwpWNYXgzJyiI/scene.splinecode" />
+      </div>
+    </>
+  );
+}
+
+export default App;
--- src/app.tsx
+++ src/app.tsx
@@ -4,16 +4,14 @@ import Spline from '@splinetool/react-spline';
 function App() {
   return (
     <>
-      <div className="display:flex min-h:100vh flex-direction:col flex-direction:row@sm">
-        <div className="py:10x px:8x w:50%@md w:100%@sm">
-          <h1 className="font-size:2.5rem">Daniel Cardenas</h1>
-          <p className="font-size:1.5rem">
-            Hi! I'm a software engineer and web developer. I'm passionate about
-            building software that is fast, reliable, and user-friendly.
-          </p>
-        </div>
-        <div className="py:10x px:8x w:50%@md w:100%@sm">
-          <Spline scene="https://prod.spline.design/KDwRgP15DHzYOVZS/scene.splinecode" />
+      <div className="display:flex h:100% flex-direction:col flex-direction:row@sm w:100% min-h:500px">
+        <header className="gradient-bg px:14px w:70%@md w:100%@sm color:white">
+          <h1 className="font-size:1.5rem">Daniel Cardenas</h1>
+        </header>
+
+        {/* CPU 3D Model */}
+        <div className="py:30x px:8px w:30%@md w:100%@sm min-h:500px max-h:500px">
+          <Spline scene="https://prod.spline.design/lI9F1J1LQf34N-nN/scene.splinecode" />
         </div>
       </div>
     </>
